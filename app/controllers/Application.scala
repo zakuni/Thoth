7e0d59d2fcbb81edf1331295314b6f603999837e
zakuni <kunio038@gmail.com>
2012-11-26 00:00:02 +0900

    index doesn't redirect to sports, made pages for each sports

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,19 +7,21 @@ import models._
 object Application extends Controller {
   
   def index = Action {
-    Redirect(routes.Application.sports)
-  }
-
-  def sports = Action { 
     val list = List(new Badminton,
                     new Futsal,
                     new Rugby)
     Ok(views.html.index(list))
   }
 
-  def rugby = TODO
+  def rugby = Action { 
+    Ok(views.html.schedule(new Rugby))
+  }
 
-  def futsal = TODO
+  def futsal = Action { 
+    Ok(views.html.schedule(new Futsal))
+  }
 
-  def badminton = TODO
+  def badminton = Action { 
+    Ok(views.html.schedule(new Badminton))
+  }
 }
