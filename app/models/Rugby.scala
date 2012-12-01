e082d4ccfa956b4641af027c523ae016733990be
zakuni <kunio038@gmail.com>
2012-12-01 16:48:25 +0900

    HTTP request in Sport class, show schedules in view

--- app/models/Rugby.scala
+++ app/models/Rugby.scala
@@ -1,29 +1,18 @@
 package models
-import dispatch._
-import java.io.InputStream
-import java.io.StringReader
-import scala.xml.Node
 import scala.xml.Text
 import scala.xml.XML
-import scala.xml.parsing.NoBindingFactoryAdapter
-import nu.validator.htmlparser.sax.HtmlParser
-import nu.validator.htmlparser.common.XmlViolationPolicy
-import org.xml.sax.InputSource
 
 class Rugby extends Sport("Rugby") {
-  val u = url("http://www.rugby-japan.jp/calendar/calendar_2012_11.html") >> {is => toNode(is)}
-  val h = new Http
-  val r = h(u)
-  println(r \\ "td")
+  val node = request("http://www.rugby-japan.jp/calendar/calendar_2012_11.html")
+  val cal_main = node \\ "div" filter (_ \ "@id" contains Text("cal_main"))
+  val trs = (cal_main \\ "tr").theSeq
 
-  def toNode(is: InputStream): Node = {
-    val hp = new HtmlParser
-    hp.setNamePolicy(XmlViolationPolicy.ALLOW)
-
-    val saxer = new NoBindingFactoryAdapter
-    hp.setContentHandler(saxer)
-    hp.parse(new InputSource(is))
-
-    saxer.rootElem
+  trs.foreach{ tr =>
+    tr.child.collect { 
+      case td if (td \ "@class" contains Text("cal_dtl")) ||
+        (td \ "@class" contains Text("cal_date")) || 
+        (td \ "@class" contains Text("cal_week")) =>
+        schedule = schedule :+ td.text
+    }
   }
 }
--- app/models/Sport.scala
+++ app/models/Sport.scala
@@ -1,9 +1,30 @@
 package models
+import java.io.InputStream
+import scala.collection.mutable._
+import scala.xml.Node
+import scala.xml.parsing.NoBindingFactoryAdapter
 import dispatch._
+import nu.validator.htmlparser.common.XmlViolationPolicy
+import nu.validator.htmlparser.sax.HtmlParser
+import org.xml.sax.InputSource
 
+class Sport(val name: String) {
+  var schedule = List[String]()
 
-case class Sport(name: String)
+  def toNode(is: InputStream): Node = {
+    val hp = new HtmlParser
+    hp.setNamePolicy(XmlViolationPolicy.ALLOW)
 
-object Sport { 
+    val saxer = new NoBindingFactoryAdapter
+    hp.setContentHandler(saxer)
+    hp.parse(new InputSource(is))
 
+    saxer.rootElem
+  }
+
+  def request(siteUrl: String): Http.HttpPackage[scala.xml.Node] = { 
+    val u = url(siteUrl) >> { is => toNode(is) }
+    val h = new Http
+    h(u)
+  }
 }
